f48f12f257392c732313e4252ed358511f17d856
Francisco Kurucz <juanfkurucz@gmail.com>
2024-05-29 15:44:48 -0300

    Non editables

--- full_install.sh
+++ full_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-python3 -m pip install -e multimodal/
-python3 -m pip install -e autogluon/
+python3 -m pip install multimodal/
+python3 -m pip install autogluon/
