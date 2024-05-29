6ade3b7e60d321791e90fe2c77d03f77ecbbe1ca
Francisco Kurucz <juanfkurucz@gmail.com>
2024-05-29 14:58:27 -0300

    Only multimodal

--- full_install.sh
+++ full_install.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-python3 -m pip install -e common/[tests]
-python3 -m pip install -e core/[all,tests]
-python3 -m pip install -e features/
-python3 -m pip install -e tabular/[all,tests]
-python3 -m pip install -e multimodal/[tests]
-python3 -m pip install -e timeseries/[all,tests]
-python3 -m pip install -e eda/
+python3 -m pip install -e multimodal/
 python3 -m pip install -e autogluon/
